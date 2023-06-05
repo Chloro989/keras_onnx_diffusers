8386775c35f6f70842e36e091f0bd626d54ef814
krkt <84625053+Chloro989@users.noreply.github.com>
2023-06-06 00:21:19 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p ~/Desktop/keras_onnx_diffusers
 cd ~/Desktop/keras_onnx_diffusers
-git clone https://github.com/Chloro989/keras_onnx_diffusers.git
+git clone https://github.com/Chloro989/keras_onnx_diffusers.git .
