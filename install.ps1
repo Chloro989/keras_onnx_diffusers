6b2647ec35236f04581df34ac62b14da76dfe0dc
Chloro989 <seisouinn@gmail.com>
2023-06-06 13:45:00 +0900

    upload install,ps1

---
+++ install.ps1
@@ -0,0 +1,3 @@
+New-Item -ItemType Directory -Force -Path "$env:USERPROFILE\Desktop\keras_onnx_diffusers"
+Set-Location -Path "$env:USERPROFILE\Desktop\keras_onnx_diffusers"
+git clone https://github.com/Chloro989/keras_onnx_diffusers.git .
